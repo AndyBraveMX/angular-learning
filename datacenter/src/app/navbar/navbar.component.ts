96d568a47763eadce417e270664ccc8d9ed63b80
andy bravo <andres.bravo@propiedades.com>
2020-02-20 13:16:43 -0600

    Creacion de los tres proyectos

---
+++ datacenter/src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  styles: [`.btn { background-color: #000; }`]
+})
+export class NavbarComponent {
+
+  @Output() onRefresh: EventEmitter<null> = new EventEmitter<null>();
+
+  refresh() {
+	this.onRefresh.emit();
+  }
+
+}
---
+++ stocks/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutes } from './app.routes';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { StocksService } from './services/stocks.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutes
+  ],
+  providers: [StocksService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
